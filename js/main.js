20c7ff7404cda02e9f1c10b07b3732b3daac2c7a
agilvarry <gilvarry@wisc.edu>
2018-05-01 19:09:14 -0600

    change
    change

--- js/main.js
+++ js/main.js
@@ -1,27 +1,8 @@
-//pseudocode - order of attack
-
-
-//TODOs
-//STYLING
-
-
-
-
-
-//write script
-
-//recactor chart creations
-//general refactoring
-//may not need to copy regions or world?? deal with in functions
-//be rid of global variables when all is said and done, including possibly the indicators variables
-
-
 //self calling global function
 (function(){
-  //global vars that i need to not be global before i turn this in
+  //these two global variables are actually bad practice. I should revisit this program and refactor it to get rid of them
   var year = 1990;
   var region = "All";
-
   //constants
   const width = window.innerWidth * 0.29,
   height = window.innerHeight * 0.33,
@@ -31,7 +12,7 @@
   chartInnerWidth = width - leftPadding - rightPadding,
   chartInnerHeight = height - topBottomPadding * 2,
   translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
-
+  //for title
   const titleInfo = {
     t5A : "Reduce Maternal Mortality Rates",
     t5B : "Access to Reproductive Healthcare",
@@ -44,7 +25,7 @@
     722 : "Share of women in non-agricultural wage employment",
     557 : "Percent of Seats held by women in national parliament"
   };
-
+  //holder for region labeling
   const regionNames ={
     All: "All UN Nations",
     Developed: '"Developed" Countries',
@@ -59,25 +40,26 @@
     SouthEastern_Asia: "South-Eastern Asia"
   }
 
-
   //create leaflet map
   function createMap(error, countries, mdg){
-    //initialize map, set coordniates and zoom
-    //value of current indicator
 
+
+    //ID of current indicator
     let indicator = $("#indicators").val();
+    //initialize variable to hold all datasets as they are generated
     let datasets = {};
+    //filter mdg csv file to the current indicator
     let current = mdg.filter(country => country.SeriesCode === indicator);
-
+    //set up map
     let myMap = L.map('map').setView([20.1657, 10.4515], 2);
     //set basemap tile layer to map
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
       attribution: 'Tiles &copy; Esri; Data: MDGI'
     }).addTo(myMap);
-    //convert topojson to geojson
+    //convert topojson to geojson, and make a copy for use in later datasets
     var copyWorld = JSON.parse(JSON.stringify(topojson.feature(countries, countries.objects.MDGCountries).features));
     let World = topojson.feature(countries, countries.objects.MDGCountries).features
-
+    //varaible to hold layers with dataset information
     let regions = {
       All: [],
       Developed: [],
@@ -99,20 +81,21 @@
 
     //split into regions
     let currentRegions = makeRegions(currentcountries, regions);
-    //group regions into object for storage
+    //group regions into object for storage, add to map
     datasets[indicator] = makeGroup(currentRegions);
     datasets[indicator][region].addTo(myMap);
+    //call all functions necessary to set up program
     updateChoropleth(myMap, datasets[indicator]);
     createSequenceControls(myMap,datasets[indicator], datasets);
     sideMap(myMap, datasets[indicator], World, datasets);
     setChart(myMap, datasets[indicator]);
     setInfo(myMap);
     indicators(mdg, copyWorld, copyRegions, myMap, datasets);
+    //makes make visible after everything is loaded
     document.getElementsByTagName("html")[0].style.visibility = "visible";
   };
-
+  //make geojson objects for leaflet and group them
   function makeGroup(regions){
-    //make geojson objects for leaflet, i can probably refacor along with changedata portion
     var  Developed = L.geoJson(regions['Developed'], {style: style, onEachFeature: onEachFeature}),
     Latin_America_Caribbean = L.geoJson(regions['Latin_America_Caribbean'], {style: style,onEachFeature: onEachFeature}),
     Northern_Africa = L.geoJson(regions['Northern_Africa'],{style: style,onEachFeature: onEachFeature}),
@@ -130,7 +113,7 @@
 
       return layers
     }
-
+    //control movement of label popup for side bars
     function moveLabel(){
       //get width of label
       var labelWidth = d3.select(".infolabel")
@@ -183,20 +166,21 @@
 
     //function to initialize D3 bar chart in side panel
     function setChart(map, layers){
-
+      //scales
       let yScale = d3.scaleLinear().range([height,0]);
       let yAxis = d3.axisLeft(yScale);
       //get current regions
-
+      //get data for current region
       let data = filterRegions(layers[region]);
 
-
+      //get max value to set domain
       let max = d3.max(data, (d) => +d[year]);
       yScale.domain([0, max]);
-
+      //remove any values that are less than zero, meaning they don't have a value
       let filtered = data.filter(d=> +d[year]>0);
-
+      //set coloscale
       var colorScale = makeColorScale(data);
+      //if there are nondatapoints display message
       if (filtered.length===0){
         var text = d3.select("#regionChart")
         .append("svg")
@@ -205,7 +189,6 @@
         text.append("text")
         .attr("transform", `translate(${height/2}, ${height/2})`)
         .text("No Data Available");
-
       }else{
         //create svg element to hold the bar chart
         var chart = d3.select("#regionChart")
@@ -213,20 +196,17 @@
         .attr("width", width)
         .attr("height", height)
         .attr("class", "chart");
-
         //add axis to chart
         var axis = chart.append("g")
         .attr("class", "axis")
         .attr("transform", translate)
         .call(yAxis);
-
         //set bars
         var bars = chart.selectAll(".bars")
         .data(filtered)
         .enter()
         .append("rect")
         .sort((a, b)=> b[year]-a[year]);
-
         bars.attr("x", (d, i) => i * (chartInnerWidth / filtered.length)+ leftPadding)
         .attr("height", d => height - yScale(parseFloat(d[year])))
         .attr("y", d => yScale(parseFloat(d[year])) +topBottomPadding)
@@ -238,7 +218,6 @@
         .on("mouseover", d => setLabel(d))
         .on("mouseout", (d) => d3.select(".infolabel").remove())
         .on("mousemove", moveLabel);
-
         //set y axis
         d3.selectAll("g.axis")
         .call(yAxis);
@@ -247,40 +226,35 @@
 
     //make popup content, want to change this so it's stationay in corner
     function createChart(attributes){
-
       let max = d3.max(attributes, d=>parseFloat(d));
       let min = d3.min(attributes, d=>parseFloat(d));
-
       var xScale = d3.scaleTime().range([0, width-40]).domain([1990, 2015]);
       let yScale = d3.scaleLinear().range([height,0]).domain([(min - (min/5)), max]);
       var xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d")).ticks(5);
       var yAxis = d3.axisLeft(yScale);
       //if the value is NaN set to 0 for
       attributes.forEach(function(item, i) { if (!isFinite(item)) attributes[i] = 0; });
-
+      //svg
       let svg = d3.select('.info')
       .select('svg')
       .attr("width", width)
       .attr("height", height)
       .attr("class", "chart")
-
       //add axis to chart
       var yAxis = svg.append('g')
       .attr('class', 'yAxis')
       .attr("transform", "translate(40, 0)")
       .call(yAxis);
-
+      //axis
       var xAxis =  svg.append('g')
       .attr("class", "xAxis")
       .attr("transform", `translate(${leftPadding}, ${height})`)
       .call(xAxis);
-
       //set bars
       var bars = svg.selectAll('.infoBars')
       .data(attributes)
       .enter()
       .append('rect');
-
       bars.attr("x", (d, i) => i * (chartInnerWidth / attributes.length)+ leftPadding)
       .attr("height", function(d){let val=height - yScale(parseFloat(d));if(val<0){val=0;}return val;}) //checks if yScale is giving negative values when input is 0, if so returns a 0
       .attr("y", d => yScale(parseFloat(d)) + topBottomPadding)
@@ -291,41 +265,37 @@
 
     //separate the world into regions
     function makeRegions(countries, regions){
-      let num = 0
+      //make copy of blank regions
       let newRegions = JSON.parse(JSON.stringify(regions));
-
       //loop through countries and group into regions object
       for (let country in countries){
-        num +=1;
         let setRegion = countries[country].properties.REGION_UN;
         newRegions[setRegion].push(countries[country]);
         newRegions["All"].push(countries[country]);
       };
-
       return newRegions;
     };
-
+    //filters countries to just the current region
     function filterRegions(layers){
       let currentRegions =[];
-
       layers.eachLayer(function(layer){
-
         if(layer.feature){
           currentRegions.push(layer.feature.properties);
         }
       })
-
       return currentRegions;
-    }
+    };
 
     //update choropleth style in map
     function updateChoropleth(map, layers){
+      //filter countries to just the current region
       let current = filterRegions(layers[region]);
       //create colorscale
       var colorScale = makeColorScale(current);
+      //set color to each country in layer
       layers[region].eachLayer(function(layer){
         if(layer.feature){
-
+          //current coutnry value
           let val = parseFloat(layer.feature.properties[year]);
           //should also have year not be global
           if(isFinite(val)){
@@ -361,7 +331,6 @@
         Eastern_Asia: "#E6CBB3",
         SouthEastern_Asia: "#F9DA06"
       };
-
       //map container
       const map = d3.select("#regionSelector")
       .append("svg")
@@ -373,7 +342,6 @@
       .center([10, 15])
       .scale(height *0.38)
       .translate([width / 2, height / 2]);
-
       //create a path to draw the geometry and set the projection
       const path = d3.geoPath()
       .projection(projection);
@@ -400,7 +368,7 @@
         }
       })
     };
-
+    //use sidebar dropdowns to control the dataset displaying in the map
     function changeData(indicator, mdg, world, regions, map, datasets){
       //if the overall dataset doesn't contain current dataset, add it
       if(!(datasets.hasOwnProperty(indicator))){
@@ -415,10 +383,8 @@
         //group regions into a set to store in overall datasets object
         datasets[indicator] =  makeGroup(currentRegions);
       };
-
       //remove every layer in map but basemap
       removeLayers(map);
-
       //add current region from current dataset to the map
       datasets[indicator][region].addTo(map);
       //update the choropleth of the map
@@ -437,15 +403,14 @@
       let indicator = $("#indicators").val();
       //gets the id the element just clicked
       region = newRegion;
-
+      //new region reigon
       let layer = datasets[indicator][region];
-
       layer.addTo(map);
+      //hardcoded zooms for a few regions
       if (region == "All" || region == "Developed"){
         map.setView([20.1657, 10.4515], 2);
       } else if(region == "Oceania"){
         map.setView([-9.6457, 160.1562], 4);
-
       } else{
         //get the extent and bounds of the new layer and zoom to it
         let extent = layer.getBounds()
@@ -480,7 +445,6 @@
     function makeColorScale(data){
       const colorClasses = ["#f2f0f7", "#cbc9e2", "#9e9ac8", "#756bb1", "#54278f"];
       //create color scale generator
-
       var colorScale = d3.scaleQuantile().range(colorClasses);
       //build array of all values of the expressed attribute
       let domainArray = [];
@@ -488,24 +452,20 @@
         let val = parseFloat(data[i][year]);
         domainArray.push(val);
       };
-
       //assign array of expressed values as scale domain
       colorScale.domain(domainArray);
       return colorScale;
     };
-
+    //join data from csv with geojson layer
     function joinData(current, countries){
       //loop through current to assign each set of current attribute values to geojson region
       for (let i=0; i<current.length; i++){
         let currentCountry = current[i]; //the current country in the CVS
         let currentCode = currentCountry.CountryCode; //the code of the current country in the current dataset
-
         //loop through geojson regions to find correct region
         for (let j=0; j<countries.length; j++){
-
           let jsonCountry = countries[j].properties; //the current region geojson properties
           let jsonCode = jsonCountry.code; //the geojson primary key code
-
           //where primary keys match, transfer current data to geojson properties object
           if (jsonCode == currentCode){
             //assign all attributes and values
@@ -515,7 +475,6 @@
           };
         };
       };
-
       return countries;
     };
 
@@ -542,9 +501,9 @@
     };
     //update the indicator selector in the side bar
     function indicators(mdg, world, regions, map, datasets){
+      //make blank copies of the region andworld objects
       var copyRegions = JSON.parse(JSON.stringify(regions));
       var copyWorld = JSON.parse(JSON.stringify(world));
-
       const indicatorList = {
         t5A : "<option value='553'>Maternal mortality ratio per 100,000 births</option> <option value='570'>% of births attended by skilled health personnel</option>",
         t5B : "<option value='730'>Contraceptive use for married women 15-49</option><option value='761'>Adolescent birth rate, per 1,000 women</option><option value='762'>% of antenatal care coverage, at least one visit</option>",
@@ -554,26 +513,21 @@
       var target = $("#targets").val();
       //value of current indicator
       var indicator = $("#indicators").val()
-
       changeTitle(target, indicator);
-
       //updates indicator/target
       $("#targets").change(function () {
-
         target = $(this).val();
         //update indicator list
-        //  map.removeLayer(datasets[indicator][region]);
         $("#indicators").html(indicatorList[target]);
-        indicator = $("#indicators").val()
-
+        indicator = $("#indicators").val();
+        //change in map data and title
         changeData(indicator, mdg, copyWorld, copyRegions, map, datasets);
         changeTitle(target, indicator);
       });
-
       //updates indicator
       $("#indicators").change(function(){
         indicator = $(this).val();
-
+        //change in map data and title
         changeData(indicator, mdg, copyWorld, copyRegions, map, datasets);
         changeTitle(target, indicator);
       })
@@ -590,7 +544,7 @@
       };
       //set up Div for popup
       var infoDiv = document.querySelector('.info');
-
+      //set popup info base on props
       if(!props){
         infoDiv.innerHTML = 'Click on a country for more information';
       }  else if(!notEmpty || !isFinite(props.SeriesCode)){
@@ -630,7 +584,6 @@
     };
     //Create new sequence controls within map   //this is only looking at initial data
     function createSequenceControls(map, layers, datasets){
-
       let SequenceControl = L.control({ position: 'bottomleft'} );
       SequenceControl.onAdd = function(map) {
         // create the control container with a particular class name
